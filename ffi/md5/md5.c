3176367676e093b2987b65bd6e3dc32684d6b273
Douglas Mennella <douglas.mennella@gmail.com>
2025-02-04 13:40:44 +0900

    already allocated

--- ffi/md5/md5.c
+++ ffi/md5/md5.c
@@ -2,5 +2,4 @@
 #include<k.h>
 
 K md5(K x){K y=KC(0,MD5_DIGEST_LENGTH);MD5_CTX ctx;MD5Init(&ctx);
-  MD5Update(&ctx,(const uint8_t *)x,NK(x));MD5Final((uint8_t*)y,&ctx);
-		   unref(x);return KC((char*)y,NK(y));}
+  MD5Update(&ctx,(const uint8_t *)x,NK(x));MD5Final((uint8_t*)y,&ctx);unref(x);return y;}
